ef9bf9352a77686d8de44d7f7bd62e2ee0c2ed0f
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-08-05 23:10:38 -0600

    topics brushed up

--- An_array_of_sequence_l/proof.py
+++ An_array_of_sequence_l/proof.py
@@ -1,7 +1,41 @@
+import math
+
 # cartesian product
+
+pens = ['Azor', 'Bic']
+colors = ['Blue', 'Black', 'Red']
+
+list_pens = [(pen,color) for pen in pens for color in colors]
+print(list_pens)
+
+# listcomps vs filter/map
+
+numbers_map = list(filter(lambda number: number, map(math.exp2,range(0,101,1))))
+print(f'Filter/Map:\n{numbers_map}')
+
+numbers_list = [ math.exp2(number) for number in range(0,101,1)]
+print(f'List comprehension:\n{numbers_list}')
 
 # listcomps
 
+odd_numbers = [last := number for number in range(0,101,1) if number%2==0]
+print(odd_numbers)
+print(f'Last element: {last}')
+
+
 # local_scope
 
+even_number = [number for number in range(0,101,1) if number%2!=0]
+print(even_number)
+
 # parrallel_assigment
+
+books = ('Python', 'Java')
+masters, beginners = books
+print(masters)
+print(beginners)
+
+first, second, *rest = range(11)
+print(first)
+print(second)
+print(rest)
